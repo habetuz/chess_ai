253593ef21b07e1ce8433687aeac5dd314a3e2c0
Marvin Fuchs <marfinn3@gmail.com>
2022-01-03 16:42:56 +0100

    Format code

--- src/io.rs
+++ src/io.rs
@@ -1,16 +1,19 @@
 use std::io::Write;
-use crate::engine;
+
 use crate::engine::GameState;
 use crate::engine::GameState::Normal;
 
 pub fn turn(board: [[char; 8]; 8], white: bool) -> GameState {
     let input = loop {
-        if white { print!("White> "); }
-        else { print!("Black> "); }
+        if white {
+            print!("White> ");
+        } else {
+            print!("Black> ");
+        }
 
         std::io::stdout().flush().expect("Could not flush stdout!");
 
-        let input =  {
+        let input = {
             let mut input = "".to_string();
             std::io::stdin()
                 .read_line(&mut input)
@@ -21,7 +24,9 @@ pub fn turn(board: [[char; 8]; 8], white: bool) -> GameState {
         let input = input.to_uppercase();
 
         match parse_input(input) {
-            Ok(value) => { break value; }
+            Ok(value) => {
+                break value;
+            }
             Err(_) => {
                 println!("Could not read input!");
                 continue;
@@ -32,21 +37,20 @@ pub fn turn(board: [[char; 8]; 8], white: bool) -> GameState {
     println!("Move {} to {}", input.0, input.1);
 
     if white {
-
     } else {
-
     }
 
     Normal(board)
 }
 
-fn parse_input<'a>(input : String) -> Result<(String, String), ()> {
+fn parse_input<'a>(input: String) -> Result<(String, String), ()> {
     let input: Vec<&str> = input.split_whitespace().collect();
 
-    if input.len() != 2 ||
-        !validate_coordinate(input[0]) ||
-        !validate_coordinate(input[1]) ||
-        input[0] == input[1]{
+    if input.len() != 2
+        || !validate_coordinate(input[0])
+        || !validate_coordinate(input[1])
+        || input[0] == input[1]
+    {
         return Err(());
     }
 
@@ -56,24 +60,22 @@ fn parse_input<'a>(input : String) -> Result<(String, String), ()> {
 fn validate_coordinate(coordinate: &str) -> bool {
     let coordinate: Vec<char> = coordinate.chars().collect();
 
-    coordinate.len() == 2 && (
-        coordinate[0] == 'A' ||
-        coordinate[0] == 'B' ||
-        coordinate[0] == 'C' ||
-        coordinate[0] == 'D' ||
-        coordinate[0] == 'E' ||
-        coordinate[0] == 'F' ||
-        coordinate[0] == 'G' ||
-        coordinate[0] == 'H'
-    ) && (
-        coordinate[1] == '1' ||
-        coordinate[1] == '1' ||
-        coordinate[1] == '2' ||
-        coordinate[1] == '3' ||
-        coordinate[1] == '4' ||
-        coordinate[1] == '5' ||
-        coordinate[1] == '6' ||
-        coordinate[1] == '7' ||
-        coordinate[1] == '8'
-    )
+    coordinate.len() == 2
+        && (coordinate[0] == 'A'
+            || coordinate[0] == 'B'
+            || coordinate[0] == 'C'
+            || coordinate[0] == 'D'
+            || coordinate[0] == 'E'
+            || coordinate[0] == 'F'
+            || coordinate[0] == 'G'
+            || coordinate[0] == 'H')
+        && (coordinate[1] == '1'
+            || coordinate[1] == '1'
+            || coordinate[1] == '2'
+            || coordinate[1] == '3'
+            || coordinate[1] == '4'
+            || coordinate[1] == '5'
+            || coordinate[1] == '6'
+            || coordinate[1] == '7'
+            || coordinate[1] == '8')
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 use crate::engine::GameState;
-
-mod io;
 mod engine;
+mod io;
 
 fn main() {
     let mut board = [[' '; 8]; 8];
     loop {
         board = match io::turn(board, true) {
-            GameState::Normal(value) => {value}
-            GameState::CheckMate(value) => {value}
-            GameState::Check(value) => {value}
+            GameState::Normal(value) => value,
+            GameState::CheckMate(value) => value,
+            GameState::Check(value) => value,
         };
         board = match io::turn(board, false) {
-            GameState::Normal(value) => {value}
-            GameState::CheckMate(value) => {value}
-            GameState::Check(value) => {value}
+            GameState::Normal(value) => value,
+            GameState::CheckMate(value) => value,
+            GameState::Check(value) => value,
         };
     }
 }
