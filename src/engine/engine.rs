843d6f3431f087b9a7a3b78bc4576f27fe723010
stetide <ameyer@energy-consulting-meyer.de>
2022-01-03 13:51:59 +0100

    first commit

---
+++ src/engine/engine.rs
@@ -0,0 +1,16 @@
+type piece = u8;
+
+pub enum Pieces {
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn,
+}
+
+impl piece {
+    fn isValidMove(self,) {
+        println!()
+    }
+}
